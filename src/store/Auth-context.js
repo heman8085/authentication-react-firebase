3f74ed61ea43c5d668cfe7c6e926c74d8e77cd50
himanshu shukla <hemi8085@gmail.com>
2024-05-03 10:18:57 +0530

    Addressing security and logout issues

--- src/store/Auth-context.js
+++ src/store/Auth-context.js
@@ -7,16 +7,18 @@ const AuthContext = React.createContext({
 });
 
 const AuthContextProvider = (props) => {
-    const [token, setToken] = useState(null)
+    const initialToken = localStorage.getItem('token');
+    const [token, setToken] = useState(initialToken)
 
     const userIsLoggedIn = !!token;
 
     const loginHandler = (newToken) => {
-
         setToken(newToken);
+        localStorage.setItem('token', token);
      };
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     };
     const contextValue = {
         token: token,
